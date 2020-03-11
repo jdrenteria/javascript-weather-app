b40d5647ca7e9b9001104ece5d935f46b546d630
Jesus Renteria <Jesus Renteria>
2020-03-11 11:39:56 -0500

    added function to console.log data, added function to connect search button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let appId = 'f6046f488dac61b61e6dc48c6c5cb236';
 let units = 'imperial';
-let searchMethod;
+let searchMethod = 'zip';
 
 function searchWeather(searchTerm){
     fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`).then(result =>{
@@ -9,3 +9,16 @@ function searchWeather(searchTerm){
         init(result);
     })
 }
+function init(resultFromServer){
+    console.log(resultFromServer);
+
+}
+//search button //
+document.getElementById('searchBtn').addEventListener('click', () => {
+    let searchTerm = document.getElementById("searchInput").value;
+   
+    if(searchTerm)
+    searchWeather(searchTerm);
+}
+
+)
